1161cae6fa9e3336a8ee250c72cd0c00ddaf1193
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-03-07 15:29:09 +0300

    commit 2

--- app/src/main/java/com/example/hostel_locator/MainActivity.kt
+++ app/src/main/java/com/example/hostel_locator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.hostel_locator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hostel_locator.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
