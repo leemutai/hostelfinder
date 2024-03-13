80a0b2900afa2a9078b3731a5f5d2d4e2af632c8
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-03-13 19:26:41 +0300

    updated home fragmen

--- app/src/main/java/com/example/hostel_locator/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/hostel_locator/Fragment/HomeFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.hostel_locator.R
+import com.example.hostel_locator.adapter.Row1Adapter
+import com.example.hostel_locator.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,6 +25,42 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return  binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner2, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner3, ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList,ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object :ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                 val itemPosition = imageList[position]
+                val itemMessage="Selected Image $position"
+                Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
+        val apartName = listOf("Wiku","Vinci","Beyas","Lato")
+        val price = listOf("Ksh6000","Ksh5000","Ksh8000","Ksh10000")
+        val rating = listOf("4.9","4.0","3.0","4.5")
+        val location = listOf("Rongai","Pangani","Gataka","Rosters")
+        val hsetype = listOf("Apartment","Bedsitter","Studio","OneBedroom")
+        val bed = listOf("3.0","1.0","1.0","1.0")
+        val apartImages = listOf(R.drawable.hostel,R.drawable.hostel2,R.drawable.hostel,R.drawable.hostel2)
+
+
+        val adapter = Row1Adapter(apartName,price,apartImages,price,rating,location,hsetype,bed)
     }
 }
---
+++ app/src/main/java/com/example/hostel_locator/adapter/Row1Adapter.kt
@@ -0,0 +1,47 @@
+package com.example.hostel_locator.adapter
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hostel_locator.databinding.FragmentHomeBinding
+import com.example.hostel_locator.databinding.Row1ItemBinding
+
+class Row1Adapter(private val items:List<String>,private val price:List<String>,private val rating:List<String>,private val location:List<String>,private val hsetype:List<String>,private val bed:List<String>,private val image:List<Int>): RecyclerView.Adapter<Row1Adapter.Row1ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Row1ViewHolder {
+        return Row1ViewHolder(Row1ItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+    }
+
+
+
+    override fun onBindViewHolder(holder: Row1ViewHolder, position: Int) {
+        val item = items[position]
+        val images = image[position]
+        val price = price[position]
+        val rating = rating[position]
+        val location = location[position]
+        val hsetype = hsetype[position]
+        val bed = bed[position]
+
+        holder.bind(item,price,rating,location,hsetype,bed,images)
+    }
+    override fun getItemCount(): Int {
+         return items.size
+    }
+    class Row1ViewHolder(private val binding: Row1ItemBinding) :RecyclerView.ViewHolder(binding.root){
+        private val imagesView = binding.imageRectangleTwelve
+        fun bind(item: String,price: String, rating:String,location:String,hsetype: String,bed:String,images: Int) {
+             binding.apartNamePopular.text = item
+            binding.txtPrice.text = price
+            binding.txtRating.text = rating
+            binding.txtLocation.text = location
+            binding.textHseType.text = hsetype
+            binding.txtBedSize.text = bed
+            imagesView.setImageResource(images)
+        }
+
+    }
+}
