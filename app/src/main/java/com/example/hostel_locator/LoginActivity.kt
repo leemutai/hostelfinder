17cb1246ec8059dc0eb811ef37f75a16dbaffdfb
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-03-17 23:46:56 +0300

    updated login and sign in

--- app/src/main/java/com/example/hostel_locator/LoginActivity.kt
+++ app/src/main/java/com/example/hostel_locator/LoginActivity.kt
@@ -1,24 +1,143 @@
 package com.example.hostel_locator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.hostel_locator.databinding.ActivityLoginBinding
+import com.example.hostel_locator.model.UserModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private   var userName: String? = null
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.loginButton.setOnClickListener {
-            val intent = Intent(this,SignActivity::class.java)
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        //initialization of firebase auth
+        auth = Firebase.auth
+        //initialize firebase database
+        database = Firebase.database.reference
+        //initialization og google
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions)
+
+        //login with email and password
+        binding.loginButton.setOnClickListener{
+            // get data from text field
+            email = binding.emailAddress.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            if (email.isBlank()|| password.isBlank()){
+                Toast.makeText(this,"Please enter all the details", Toast.LENGTH_SHORT).show()
+            } else{
+                createUser()
+                Toast.makeText( this,"Login successful", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.donthavebutton.setOnClickListener{
+            val intent = Intent(this, SignActivity::class.java)
             startActivity(intent)
         }
-        binding.donthavebutton.setOnClickListener {
-            val intent = Intent(this,SignActivity::class.java)
-            startActivity(intent)
+        //google signIn
+        binding.googleButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            launcher.launch(signInIntent)
+
         }
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if (task.isSuccessful) {
+                val account: GoogleSignInAccount? = task.result
+                val credential: AuthCredential =
+                    GoogleAuthProvider.getCredential(account?.idToken, null)
+                auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText( this,"Sign In succesful", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Sign In failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        else {
+            Toast.makeText(this, "Sign In failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun createUser() {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val user: FirebaseUser? = auth.currentUser
+                updateUi(user)
+            } else{
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        saveUserdata()
+                        val user:FirebaseUser? = auth.currentUser
+                        updateUi(user)
+                    } else{
+                        Toast.makeText(this,"Sign-in failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveUserdata() {
+        // get data from text field
+
+        email = binding.emailAddress.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user = UserModel(userName,email,password)
+        val userId:String = FirebaseAuth.getInstance().currentUser!!.uid
+        //save data imto dataase
+        database.child("user").child(userId).setValue(user)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser: FirebaseUser? = auth.currentUser
+        if (currentUser!= null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+    private fun updateUi(user: FirebaseUser?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
